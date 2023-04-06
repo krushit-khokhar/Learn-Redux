f21e19fbd344b4ec58fa7659ff906d0ac1cfa280
krushitkumar <khokharkrushit123@gmail.com>
2023-04-06 14:24:30 +0530

    create react-redux-1

---
+++ react-redux-1/src/state/reducers/amountReducers.js
@@ -0,0 +1,12 @@
+const reducer = (state=0, action) => {
+    if(action.type === 'deposit'){
+        
+        return state + action.payload
+    }else if(action.type === 'withdraw'&&state>0){
+        return state - action.payload
+    }else{
+        return state
+    }
+
+}
+export default reducer;
